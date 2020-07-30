40d87eb909d279022e787a641da966bb034b4265
GHMSOFT <GHMSOFT@DESKTOP-CCH3NIS>
2020-07-30 14:18:04 +0700

    first deploy

--- src/features/Photo/components/PhotoList/index.js
+++ src/features/Photo/components/PhotoList/index.js
@@ -1,42 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Col, ButtonGroup, Button } from "reactstrap";
+import { Col, ButtonGroup, Button, Spinner } from "reactstrap";
 import ModalDelete from "../ModalDelete";
 
 const PhotoList = (props) => {
+  const [numberOfPhoto, setNumberOfPhoto] = useState(6);
+  const [loading, setLoading] = useState(false)
+
   const { photos, onPhotoEditClick, onPhotoRemoveClick } = props;
 
-  return (
-    <div className="photo-list">
-      {photos.map((photo, i) => {
-        return (
-          <div className="photo-item" key={i}>
-            <Col>
-              {" "}
-              <img src={photo.photo} alt={photo.title} />
-              <div className="overlay">
-                <div className="text">{photo.title}</div>
-                <ButtonGroup>
-                  <Button
-                    className="mr-3"
-                    outline
-                    color="info"
-                    onClick={() => onPhotoEditClick(photo.id)}
-                  >
-                    Edit
-                  </Button>
+  const loadMorePhoto = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setNumberOfPhoto(numberOfPhoto + 6);
+      setLoading(false);
+    }, 600);
+  };
 
-                  <ModalDelete
-                    id={photo.id}
-                    onPhotoRemoveClick={onPhotoRemoveClick}
-                  />
-                </ButtonGroup>
-              </div>
-            </Col>
-          </div>
-        );
-      })}
-    </div>
+  return (
+    <>
+      <div className="photo-list">
+        {photos.slice(0, numberOfPhoto).map((photo, i) => {
+          return (
+            <div className="photo-item" key={i}>
+              <Col>
+                {" "}
+                <img src={photo.photo} alt={photo.title} />
+                <div className="overlay">
+                  <div className="text">{photo.title}</div>
+                  <ButtonGroup>
+                    <Button
+                      className="mr-3"
+                      outline
+                      color="info"
+                      onClick={() => onPhotoEditClick(photo.id)}
+                    >
+                      Edit
+                    </Button>
+
+                    <ModalDelete
+                      id={photo.id}
+                      onPhotoRemoveClick={onPhotoRemoveClick}
+                    />
+                  </ButtonGroup>
+                </div>
+              </Col>
+            </div>
+          );
+        })}
+      </div>
+      {numberOfPhoto >= photos.length ? (
+        ""
+      ) : (
+        <Button className="mt-4" onClick={loadMorePhoto}>
+         {loading && <Spinner size="sm" />} {" "}
+         {loading ? "Loading..." : "Load more"}
+        </Button>
+      )}
+    </>
   );
 };
 
